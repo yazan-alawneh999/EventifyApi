a6a85337a6fd5b33a464fbe29702f24fbb31a0f6
ABIAlzoubi <anas.basam.525@gmail.com>
2025-04-17 18:01:40 +0300

    adding discount controller

---
+++ LearningHub.Core/Repository/IDiscountsRepository.cs
@@ -0,0 +1,15 @@
+﻿using LearningHub.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningHub.Core.Repository
+{
+    public interface IDiscountsRepository
+    {
+        public List<DiscountDto> GetDiscountsByUserID(decimal userID);
+        public DiscountDto GetDiscountsByUserAndCode(decimal userID, String Code);
+    }
+}
---
+++ LearningHub.Infra/Repository/DiscountsRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using LearningHub.Core.Common;
+using LearningHub.Core.Dto;
+using LearningHub.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningHub.Infra.Repository
+{
+    public class DiscountsRepository: IDiscountsRepository
+    {
+
+        private readonly IDbContext _dbContext;
+        public DiscountsRepository(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<DiscountDto> GetDiscountsByUserID(decimal userID)
+        {
+            var p = new DynamicParameters();
+            p.Add("p_UserID", userID, dbType: DbType.Int32);
+            var result = _dbContext.DbConnection.Query<DiscountDto>("Discount_Package.GetDiscountsByUserID",p, commandType: CommandType.StoredProcedure).ToList();
+            return result;
+        }
+
+        public DiscountDto GetDiscountsByUserAndCode(decimal userID, String Code) {
+            var p = new DynamicParameters();
+            p.Add("p_UserID", userID, dbType: DbType.Int32);
+            p.Add("p_DiscountCode", Code, dbType: DbType.String);
+            var result = _dbContext.DbConnection.Query<DiscountDto>("Discount_Package.GetDiscountsByUserAndCode", p, commandType: CommandType.StoredProcedure).SingleOrDefault();
+            return result;
+        }
+    }
+}
